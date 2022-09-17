c7ce038ced59bcd0a2f505e252beee49f19f2ae9
choisungwoo <sqlstyle@naver.com>
2022-09-17 09:30:52 +0900

    1181문제 완료.

--- 1181/solution.js
+++ 1181/solution.js
@@ -1,2 +1,15 @@
-const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString();
+function solution() {
+    const input = ['but','i','wont','hesitate','no','more','no','more','it','cannot','wait','im','yours']
+
+    //set으로 중복 단어 제거
+    Array.from(new Set(input))
+    //사전순으로 한번 정렬
+    .sort()
+    //길이순으로 정렬
+    .sort((a,b) => a.length - b.length)
+    //하나씩 출력.
+    .forEach(i => console.log(i))
+
+}
+
+solution()
