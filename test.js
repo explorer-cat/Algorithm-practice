44012308764ff636fd591865f7a557c8aeab1ed8
choisungwoo <sqlstyle@naver.com>
2022-09-17 11:03:28 +0900

    백준 1152.

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString();
 
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+    let count = 0;
+    let word = input.trim().split(' ');
 
-let input = []; // input 배열 선언
-
-rl.on('line', function(line){
-    input.push(line); // 입력받는 각 줄의 값을 input 배열에 저장
-}).on('close', function(){
-    Array.from(new Set(input)).sort().sort((a,b) => a.length - b.length).forEach(i => console.log(i))
-    process.exit();
-})
+    for(let i = 0; i < word.length; i++) {
+        if(word[i] !== ''){
+            count++;
+        }
+    }
+    console.log(count)
